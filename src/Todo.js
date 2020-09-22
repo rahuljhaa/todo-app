ec5ed9cdcdb2be6f78a749745095623bf5e00bd4
shubhankarb180 <shubhankarb180@gmail.com>
2020-09-22 22:31:11 +0530

    code: Refractor Todo Component

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Todo(props) {
+function Todo({text}) {
     return (
         <div>
-            <li>{props.text}</li>
+            <li>{text}</li>
         </div>
     )
 }
